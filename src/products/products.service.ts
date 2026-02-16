c1d9656adc5ff98644de4f64ab685087b8f7b0e9
Andrew <andrewgalaksy@gmail.com>
2026-02-17 02:09:04 +0300

    Написана логика для products, crud товаров работает

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,71 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Category } from '../categories/category.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from './product.entity';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+    @InjectRepository(Category)
+    private readonly categoryRepository: Repository<Category>,
+  ) {}
+
+  async create(dto: CreateProductDto) {
+    const category = await this.categoryRepository.findOne({
+      where: { id: dto.categoryId },
+    });
+
+    if (!category) {
+      throw new NotFoundException('Category not found');
+    }
+
+    const product = this.productRepository.create({
+      name: dto.name,
+      description: dto.description,
+      price: dto.price,
+      stock: dto.stock,
+      category,
+    });
+
+    return await this.productRepository.save(product);
+  }
+
+  async findAll() {
+    return await this.productRepository.find({
+      relations: ['category'],
+    });
+  }
+
+  async findOne(id: number) {
+    const product = await this.productRepository.findOne({
+      where: { id },
+      relations: ['category'],
+    });
+
+    if (!product) {
+      throw new NotFoundException('Product not found');
+    }
+
+    return product;
+  }
+
+  async update(id: number, dto: UpdateProductDto) {
+    const product = await this.findOne(id);
+
+    Object.assign(product, dto);
+
+    return await this.productRepository.save(product);
+  }
+
+  async remove(id: number) {
+    const product = await this.findOne(id);
+    await this.productRepository.remove(product);
+
+    return { message: 'Product deleted' };
+  }
+}
